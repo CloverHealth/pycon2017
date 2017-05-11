56e048aa1680b97148e9ec90940b7cbefc0c023e
Bijan Vakili <bijan.vakili@cloverhealth.com>
2017-05-10 17:07:55 -0700

    - Add notebook of core techniques - Rearranged transformers module for better clarity

--- app/etl/transformers.py
+++ app/etl/transformers.py
@@ -10,27 +10,12 @@ LOGGER = logging.getLogger(__name__)
 NODE_PATH_CACHE_SIZE = 16  # should be a power of 2
 
 
-def transform_submissions(session, submissions, processed_on:datetime.datetime=None, to_dict=False):
-    """
-    Transforms Submissions into ResponseEvents
-
-    :param session: SQLAlchemy session
-    :param submissions: submissions generator
-    :param processed_on: optional timestamp to apply to 'processed_on' column of all ResponseEvents
-    :return: generator of ResponseEvents
-    """
-    processed_on = processed_on or utc_now()
-    get_node_path_map = get_node_path_map_cache(session)
-    num_submissions = 0
-    for submission in submissions:
-        yield from _transform_submission(get_node_path_map, submission, processed_on, to_dict)
-        num_submissions += 1
-    LOGGER.info('Transformed %d JSON submissions', num_submissions)
+NodeInfo = namedtuple('NodeInfo', ['answer_type', 'tag'])
 
 
 def map_nested(node:dict, gen_items, gen_children):
     """
-    Recursively transforms a nested python dictionary into a generator of items
+    Transforms a nested python dictionary into a generator of items
 
     :param obj: current object node to transform
     :param path: path components
@@ -43,6 +28,15 @@ def map_nested(node:dict, gen_items, gen_children):
 
 
 def _recursive_map_nested(node:dict, path: list, gen_items, gen_children):
+    """
+    NOTE: This is the core traversal algorithm (recursive depth first search with pre-order traversal)
+
+    Recursive:  The same function is called on nested subtrees
+
+    Pre-order traversal: Inspect a node's items first *before* considering its children
+
+    Depth first search: When you recurse, explore as far down a child's tree before backtracking
+    """
     if not node:
         return
 
@@ -54,7 +48,7 @@ def _recursive_map_nested(node:dict, path: list, gen_items, gen_children):
 
 def _make_path_str(path: list) -> str:
     """
-    Converts path components to a dot-separated string
+    Converts a path component list to a dot-separated string
 
     :param path: path components
     :returns: dot separated path string
@@ -62,23 +56,37 @@ def _make_path_str(path: list) -> str:
     return '.'.join(path)
 
 
-NodeInfo = namedtuple('NodeInfo', ['answer_type', 'tag'])
-
 def _load_node_path_map(session, form_id) -> dict:
     """
     Loads and constructs a information map used for transforming nested object nodes
+
+    NOTE: This is the first application of our generic map_nested() routine
 
     :param session: SQLAlchemy session
     :param form_id: Form.id
     :returns: dictionary of node path strings to NodeInfo instances
     """
+
+    # we use inner methods (closures) here rather than partial methods just to better
+    # because argument freezing is not required and it also shows that that the complexity
+    # lies in your function arguments to map_nested() and not the traversal algorithm itself
+
     def _node_info(node: dict, path: list):
+        """
+        for node's with answer types, just extract one item
+        -> (schema_path, answerType, tag)
+        """
         answer_type = node.get('answerType')
         if answer_type:
-            # schema_path, answerType, tag
             yield _make_path_str(path), constants.AnswerType[answer_type], node.get('tag')
 
     def _children(node: dict, path: list):
+        """
+        schema's children are in the item keyed as 'children'
+
+        each child is uniquely identified by the 'slug'
+        so the 'slug' is appended to the end of the current node's path
+        """
         node_slug = node.get('slug')
         children = node.get('children')
         if node_slug and children:
@@ -114,7 +122,29 @@ def get_node_path_map_cache(session):
     return cached_wrapper(_get_node_path_map)
 
 
+def transform_submissions(session, submissions, processed_on:datetime.datetime=None, to_dict=False):
+    """
+    Transforms Submissions into ResponseEvents
+
+    :param session: SQLAlchemy session
+    :param submissions: submissions generator
+    :param processed_on: optional timestamp to apply to 'processed_on' column of all ResponseEvents
+    :return: generator of ResponseEvents
+    """
+    processed_on = processed_on or utc_now()
+    get_node_path_map = get_node_path_map_cache(session)
+    num_submissions = 0
+    for submission in submissions:
+        yield from _transform_submission(get_node_path_map, submission, processed_on, to_dict)
+        num_submissions += 1
+    LOGGER.info('Transformed %d JSON submissions', num_submissions)
+
+
 def _make_output_dictionary(answer_type=None, **kwargs):
+    """
+    Used only if we watch to return pure python dictonaries rather than
+    ResponseEvent model instances
+    """
     return {
         'answer_type': answer_type.name,
         **kwargs
@@ -125,6 +155,18 @@ def _transform_submission(f_get_node_path_map,
                           submission: models.Submission,
                           processed_on:datetime.datetime,
                           to_dict:bool):
+    """
+    This is a second application of map_nested()
+
+    Instead of a schema, we traverse the *submission* nested structure instead.  Items and children
+    are structured differently in the response because each key in the tree is its own node.  Nested child
+    nodes are now any dictionary value found in the tree.
+
+    Here we don't use inner methods.  Instead, we use partial functions just once to freeze the node path map
+    for items generator that extracts individual answers.  Both functions are immediately following this function.
+    """
+
+    # these kwargs are the same for all ResponseEvents.  so just construct it once
     common_kwargs = {
         'processed_on': processed_on,
         'form_id': submission.form_id,
@@ -141,13 +183,19 @@ def _transform_submission(f_get_node_path_map,
         'user_id': submission.user.id,
         'user_full_name': submission.user.full_name,
     }
+
+    # setup the generator arguments for map nested
     node_map = f_get_node_path_map(submission.form_id)
+    f_extract_answers = functools.partial(_extract_answers, node_map=node_map)
+
+    # final conversion is usually a ResponseEvent.  however, we allow generating plain dictionaries
+    # if specified for performance purposes
     if to_dict:
         output_mapper = _make_output_dictionary
     else:
         output_mapper = models.ResponseEvent
 
-    for path, answer in _flatten_responses(submission.responses, node_map):
+    for path, answer in map_nested(submission.responses, f_extract_answers, _dict_children):
         yield output_mapper(
             schema_path=path,
             value=answer['value'],
@@ -157,23 +205,27 @@ def _transform_submission(f_get_node_path_map,
         )
 
 
-def _flatten_responses(responses: dict, node_map: dict):
-    def _answers(node: dict, path: list):
-        for k, v in node.items():
-            if not isinstance(v, (dict, list,)):
-                path_str = _make_path_str(path + [k])
-                node_info = node_map.get(path_str)
-                if node_info:
-                    # NOTE: for this workshop, we assume all date answer types are properly formatted
-                    if node_info.answer_type == constants.AnswerType.boolean:
-                        str_value = 'true' if v else 'false'
-                    else:
-                        str_value = str(v)
-                    yield path_str, {'answer_type': node_info.answer_type, 'value': str_value, 'tag': node_info.tag}
+def _dict_children(node: dict, path: list):
+    """ extracts child trees from a plain python dictionary """
+    for k, v in node.items():
+        if isinstance(v, dict):
+            yield path + [k], v
 
-    def _children(node: dict, path: list):
-        for k, v in node.items():
-            if isinstance(v, dict):
-                yield path + [k], v
 
-    yield from map_nested(responses, _answers, _children)
+def _extract_answers(node: dict, path: list, node_map=None):
+    """ converts scalar dictionary items to response event arguments reflecting answers """
+    assert node_map
+
+    for k, v in node.items():
+        if not isinstance(v, (dict, list,)):
+            path_str = _make_path_str(path + [k])
+            node_info = node_map.get(path_str)
+
+            if node_info:
+                # NOTE: for this workshop, we assume all date answer types are properly formatted
+                if node_info.answer_type == constants.AnswerType.boolean:
+                    str_value = 'true' if v else 'false'
+                else:
+                    str_value = str(v)
+
+                yield path_str, {'answer_type': node_info.answer_type, 'value': str_value, 'tag': node_info.tag}
